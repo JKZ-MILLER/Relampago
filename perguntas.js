f5bad136682e6334631dc94c3a283abd2fb650fd
JKZ-MILLER <juancarlosdasilva2021@gmail.com>
2024-11-29 17:57:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Carros 2',
+    'Quem é o protagonista de Carros 2?',
+    'O protagonista é o Relâmpago McQueen, um carro de corrida vermelho famoso por sua velocidade.'
+);
+
+criaCartao(
+    'Carros 2',
+    'Qual é a missão de Finn McMissile?',
+    'Finn McMissile é um espião internacional que trabalha para impedir um plano de sabotagem durante o Grand Prix Mundial.'
+);
+
+criaCartao(
+    'Carros 2',
+    'Quem é o melhor amigo do Relâmpago McQueen?',
+    'Seu melhor amigo é o Mate, um guincho simpático e leal que ajuda em todas as aventuras.'
+);
+
+criaCartao(
+    'Carros 2',
+    'Qual é o evento principal do filme?',
+    'O evento principal é o Grand Prix Mundial, uma corrida para determinar o carro mais rápido do mundo.'
+);
